34e529aa5d99683f5660f70146f7b9da61a6a8ac
Carlos2711 <ca.lopez@globant.com>
2016-10-24 11:56:19 -0300

    LopezCarlosTopic4Exercise1to3

--- 04-HTML5-APIs/exercise2/Scripts/script.js
+++ 04-HTML5-APIs/exercise2/Scripts/script.js
@@ -7,7 +7,7 @@ function saveText ( text ) {
   variable = localStorage.getItem("text");
   console.log(variable);
 
-  //usign indexDB
+  //usign IDB
   let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
 
   if (!window.indexedDB) {
@@ -28,6 +28,34 @@ function saveText ( text ) {
     var index = store.index("NameIndex");
 
     store.put({id: 1 , string: text});
+    // Close the db when the transaction is done
+    tx.oncomplete = function() {
+        db.close();
+    };
+  }
+
+}
+
+function erasedData(){
+  //clear localStorage
+  localStorage.removeItem("text");
+  //clear IDB
+  let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+
+  if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB.")
+  }
+
+  let open = indexedDB.open("MyIndexedDatabase", 1);
+  open.onupgradeneeded = function() {
+    let db = open.result;
+    let store = db.createObjectStore("MyObjectStore", {keyPath: "id"});
+    let index = store.createIndex("NameIndex", ["string"]);
+  };
+  open.onsuccess = function() {
+    // Start a new transaction
+    var db = open.result;
+    var tx = db.transaction("MyObjectStore", "readwrite").objectStore("MyObjectStore").clear();
     // Close the db when the transaction is done
     tx.oncomplete = function() {
         db.close();
---
+++ 04-HTML5-APIs/exercise3/Scripts/script.js
@@ -0,0 +1,84 @@
+function saveText ( text ) {
+  //using localStorage
+  let variable;
+  // method for save text
+  localStorage.setItem("text", text);
+  // method for get a item for the table
+  variable = localStorage.getItem("text");
+  console.log(variable);
+
+  //usign IDB
+  let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+
+  if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB.")
+  }
+
+  let open = indexedDB.open("MyIndexedDatabase", 1);
+  open.onupgradeneeded = function() {
+    let db = open.result;
+    let store = db.createObjectStore("MyObjectStore", {keyPath: "id"});
+    let index = store.createIndex("NameIndex", ["string"]);
+  };
+  open.onsuccess = function() {
+    // Start a new transaction
+    var db = open.result;
+    var tx = db.transaction("MyObjectStore", "readwrite");
+    var store = tx.objectStore("MyObjectStore");
+    var index = store.index("NameIndex");
+
+    store.put({id: 1 , string: text});
+    // Close the db when the transaction is done
+    tx.oncomplete = function() {
+        db.close();
+    };
+  }
+
+}
+
+function erasedData(){
+  //clear localStorage
+  localStorage.removeItem("text");
+  //clear IDB
+  let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+
+  if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB.")
+  }
+
+  let open = indexedDB.open("MyIndexedDatabase", 1);
+  open.onupgradeneeded = function() {
+    let db = open.result;
+    let store = db.createObjectStore("MyObjectStore", {keyPath: "id"});
+    let index = store.createIndex("NameIndex", ["string"]);
+  };
+  open.onsuccess = function() {
+    // Start a new transaction
+    var db = open.result;
+    var tx = db.transaction("MyObjectStore", "readwrite").objectStore("MyObjectStore").clear();
+    // Close the db when the transaction is done
+    tx.oncomplete = function() {
+        db.close();
+    };
+  }
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  let datatransf = event.dataTransfer;
+  let file = datatransf.files[0];
+  let fileReader = new FileReader();
+  fileReader.readAsText(file);
+  fileReader.onloadend = function() {
+    document.getElementById("droptextarea").innerHTML = fileReader.result;
+    drop.innerHTML = "File " + file.name + " opened";
+  };
+}
