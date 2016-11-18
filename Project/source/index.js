4fa1b50339243c79c76f2ae370ddaf2dfb4eb8ff
Carlos2711 <ca.lopez@globant.com>
2016-11-18 12:51:02 -0300

    LopezCarlosProject starting

--- Project/source/index.js
+++ Project/source/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
-import { Store } from './Scripts/Store.js';
+
 import { Home } from './Scripts/Home.js';
-import { Provider } from 'react-redux';
+
 
 class Container extends React.Component {
     constructor (props) {
@@ -20,11 +20,11 @@ class Container extends React.Component {
 };
 
 ReactDOM.render((
-  <Provider store = {Store}>
+
     <Router history = {browserHistory}>
       <Route path = '/' component = { Container } >
         <Route path = 'home' component = {Home} />
       </Route>
     </Router>
-  </Provider>
+
 ), document.getElementById('app'));
