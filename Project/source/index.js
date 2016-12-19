35f6ac0e40dd0eac59ef882a9fb9b32c23313529
Carlos2711 <ca.lopez@globant.com>
2016-12-19 13:04:41 -0300

    LopezCarlosProject adding material-ui

--- Project/source/index.js
+++ Project/source/index.js
@@ -1,12 +1,23 @@
-import  Home  from './Scripts/Home.js';
-import  ListGame  from './Scripts/ListGame.js';
-import  SaveGame  from './Scripts/SaveGame.js';
-import  Search  from './Scripts/Search.js';
-import  Store  from './Scripts/Store.js';
-import React from 'react';
-import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import FlatButton from 'material-ui/FlatButton';
+import FontIcon from 'material-ui/FontIcon';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Home  from './Scripts/Home.js';
+import ListGame  from './Scripts/ListGame.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import muiTheme from './utils/MuiTheme';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SaveGame  from './Scripts/SaveGame.js';
+import Search  from './Scripts/Search.js';
+import Store  from './Scripts/Store.js';
+
+
 
 class Container extends React.Component {
     constructor (props) {
@@ -15,14 +26,28 @@ class Container extends React.Component {
 
     render() {
         return (
-            <div className="header">
-                <Link to='/home'> Home </Link>
-                <Link to='/search'> Search  </Link>
-                <Link to='/list'> List </Link>
-                {this.props.children}
-            </div>
+            <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+                <div className="header" style={{backgroundColor: '#000000'}}>
+                    <Toolbar style={{backgroundColor: '#1a1a1a'}}>
+                        <ToolbarGroup>
+                            <img className="imagehongo" src="http://vignette1.wikia.nocookie.net/nintendo/images/9/9c/Artwork_-_SUPER_MUSHROOM_(Simple).svg/revision/latest?cb=20160716213644&path-prefix=en"/>
+                        </ToolbarGroup>
+                        <ToolbarGroup>
+                            <FlatButton label="Home" style={{color: '#39e600'}}/>
+                            <FlatButton label="Search" primary={true} style={{color: '#39e600'}}/>
+                            <FlatButton label="List" secondary={true} style={{color: '#39e600'}}/>
+                        </ToolbarGroup>
+                    </Toolbar>
+                    <Link to='/home'> Home </Link>
+                    <Link to='/search'> Search  </Link>
+                    <Link to='/list'> List </Link>
+                    {this.props.children}
+                </div>
+            </MuiThemeProvider>
+
         );
     }
+
 };
 
 ReactDOM.render((
---
+++ Project/source/utils/MuiTheme.js
@@ -0,0 +1,5 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+const MuiTheme = getMuiTheme(darkBaseTheme);
+
+export default MuiTheme;
